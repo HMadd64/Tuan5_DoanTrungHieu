8b3384b931ec9530397227765e321d5bacc64477
Trung Hiếu <106734395+HMadd64@users.noreply.github.com>
2022-06-16 15:43:00 +0700

    done lab3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tuan5_DoanTrungHieu.Models;
+using PagedList;
 
 namespace Tuan5_DoanTrungHieu.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index(int? page)
         {
-            return View();
+            if (page == null) page = 1;
+            var all_sach = (from s in data.Saches select s).OrderBy(m => m.masach);
+            int pageSize = 3;
+            int pageNum = page ?? 1;
+
+            return View(all_sach.ToPagedList(pageNum, pageSize));
         }
 
+        public ActionResult Detail(int id)
+        {
+            var D_sach = data.Saches.Where(m => m.masach == id).First();
+            return View(D_sach);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
