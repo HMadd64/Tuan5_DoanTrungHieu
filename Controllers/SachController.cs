0ea2f97d4057834832cfbcee6047c06b8c586452
Trung Hiếu <106734395+HMadd64@users.noreply.github.com>
2022-06-23 15:37:17 +0700

    1911062426_DoanTrungHieu_ChamBai

--- Controllers/SachController.cs
+++ Controllers/SachController.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Tuan5_DoanTrungHieu.Models;
+using PagedList;
 
 namespace Tuan5_DoanTrungHieu.Controllers
 {
     public class SachController : Controller
     {
         MyDataDataContext data = new MyDataDataContext();   
-        public ActionResult ListSach()
+        public ActionResult ListSach(int ? page)
         {
-            var s = from sach in data.Saches select sach;
-            return View(s);
+            if (page == null) page = 1;
+            var all_sach = (from s in data.Saches select s).OrderBy(m => m.masach);
+            int pageSize = 3;
+            int pageNum = page ?? 1;
+
+            return View(all_sach.ToPagedList(pageNum, pageSize));
         }
         public ActionResult Detail(int id)
         {
